3aaf98c8a704b351af66dc1466d68734d04163fa
20226689 <20226689@s.ubaguio.edu>
2025-10-02 19:31:55 +0800

    Integration

---
+++ rog-app-code-with-me/src/students/students-controller.js
@@ -0,0 +1,36 @@
+﻿const models = require('../../models');
+const { faker } = require('@faker-js/faker');
+
+async function getAllStudents(req, res) {
+  const students = await models['students'].findAll();
+
+  res.send(students);
+}
+
+async function getAllStudentsWithGraduateInfo(req, res) {
+  const students = await models['students'].findAll({
+    include: [
+      {
+        model: models['graduates'],
+        include: models['programs'],
+      },
+    ],
+  });
+
+  res.send(students);
+}
+
+async function createStudent(req, res) {
+  const student = await models['students'].create({
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  });
+
+  res.send(student);
+}
+
+module.exports = {
+  getAllStudents,
+  getAllStudentsWithGraduateInfo,
+  createStudent,
+};
